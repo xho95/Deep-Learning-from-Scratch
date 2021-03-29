0cd115dbbf5830a9f3e62047929ca9d1cf4aa1e1
Kim, Min Ho <xho1995@gmail.com>
2021-03-29 14:49:29 +0900

    initials the repository

---
+++ 02. Perceptron/Perceptron/Perceptron/PerceptronApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerceptronApp.swift
+//  Perceptron
+//
+//  Created by Kim, Min Ho on 2021/03/29.
+//
+
+import SwiftUI
+
+@main
+struct PerceptronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
